e9f2ad2590f2a45e62f9ed58c52528c58a36896a
MDerv <dsharma21@kentdenver.org>
2020-11-30 09:46:01 -0700

    added methods for QueueStack

--- src/QueueStack.java
+++ src/QueueStack.java
@@ -1,5 +1,51 @@
+/*
+Dhruv Sharma
+11/30/2020
+11/30/2020
+This is a the class that defines a queue using stacks.
+ */
+
+public class QueueStack<T> {
 
 
-public class QueueStack {
+    private int length;
+    private Stack firstStack;
+    private Stack secondStack;
+
+
+    public QueueStack() {
+
+    }
+
+    public void arrangeStack() { //enters firstStack into secondStack backwards to make it easier to manage
+
+    }
+
+
+    public boolean isEmpty() { //checks if stack is empty
+        if (length == 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+
+    public void enqueue(T element) { //add an element
+
+        }
+    }
+
+    public T dequeue() { //remove and return the first element
+
+        }
+    }
+
+    public int size() {
+        return secondStack.size();
+    }
+
+    public T peek() { //look at the top element without removing
+    }
 
 }
